9f7664680c84eed75b2e82003d65b90b34873167
self-destructed <self.destructed.dan@gmail.com>
2026-06-24 21:03:10 +0300

    feat(components): add LocalizedLink component

---
+++ frontend/src/components/LocalizedLink/LocalizedLink.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link';
+import { headers } from 'next/headers';
+
+type LocalizedLinkProps = Omit<React.ComponentProps<typeof Link>, 'href'> & { href: string };
+
+export default async function LocalizedLink({
+  href,
+  children,
+  ...rest
+}: LocalizedLinkProps) {
+  const headerList = await headers();
+  const lng = headerList.get('x-i18next-current-language') || 'en';
+  const localizedHref = href.startsWith('/') ? `/${lng}${href}` : href;
+
+  return (
+    <Link href={localizedHref} {...rest}>
+      {children}
+    </Link>
+  );
+}
