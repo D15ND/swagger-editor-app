91dfec1fb8d27b30e3bcea2bec18ba5152a855ca
ILYA <106765406+D15ND@users.noreply.github.com>
2026-06-25 02:26:07 +0300

    chore: add some imports from UI-kit

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,3 +1,5 @@
+import '@gravity-ui/uikit/styles/fonts.css';
+import '@gravity-ui/uikit/styles/styles.css';
 import type { Metadata } from 'next';
 import './globals.css';
 import { Providers } from './Providers';
