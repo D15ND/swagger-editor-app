167f3ad31a1781f0ebecf145b5922f14b6406870
ILYA <106765406+D15ND@users.noreply.github.com>
2026-06-24 23:43:45 +0300

    refactor: remove basic files & add rules for next config & add themeProvider

---
+++ frontend/app/themeProvider.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { ThemeProvider } from '@gravity-ui/uikit';
+import '@gravity-ui/uikit/styles/styles.css';
+import { useState } from 'react';
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+}
