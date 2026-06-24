e457e600b000d5d360e9730fc160042143a2d11c
self-destructed <self.destructed.dan@gmail.com>
2026-06-24 21:02:57 +0300

    chore(i18n): extract locales to shared config

--- frontend/src/i18n/config.ts
+++ frontend/src/i18n/config.ts
@@ -1,8 +1,9 @@
 import type { I18nConfig } from 'next-i18next/proxy';
 import resourceLoader from './loader';
+import { supportedLngs } from './locales';
 
 const i18nConfig: I18nConfig = {
-  supportedLngs: ['en', 'ru'],
+  supportedLngs,
   fallbackLng: 'en',
   defaultNS: 'common',
   ns: ['common', 'home'],
