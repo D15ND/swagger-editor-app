65a5924c17f8b693669a0a6e6a1c0def7eb7f98a
Fayzullo05 <sharifxonovfayzullo@gmail.com>
2026-06-27 16:09:32 +0500

    feat: apply shared style to about page

--- frontend/src/i18n/config.ts
+++ frontend/src/i18n/config.ts
@@ -6,7 +6,7 @@ const i18nConfig: I18nConfig = {
   supportedLngs,
   fallbackLng: 'en',
   defaultNS: 'common',
-  ns: ['common', 'home'],
+  ns: ['common', 'home', 'about'],
   reloadOnPrerender: process.env.NODE_ENV === 'development',
   resourceLoader,
 };
