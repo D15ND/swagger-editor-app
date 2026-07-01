e74d9b1dff122a29f129dc79f019d65f7001ce65
self-destructed <self.destructed.dan@gmail.com>
2026-07-01 20:37:43 +0300

    feat(components): add MethodBadge component

---
+++ frontend/src/components/History/HistoryTable/MethodBadge/MethodBadge.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import type { HttpMethod } from '@/lib/types';
+import { getMethodColor, getMethodBg } from '@/lib/colors';
+import styles from './MethodBadge.module.css';
+
+type MethodBadgeProps = {
+  method: HttpMethod;
+};
+
+export default function MethodBadge({ method }: MethodBadgeProps) {
+  return (
+    <span
+      className={styles.badge}
+      style={{
+        color: getMethodColor(method),
+        backgroundColor: getMethodBg(method),
+      }}
+    >
+      {method}
+    </span>
+  );
+}
