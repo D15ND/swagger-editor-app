9fcd5d7bdeae846e6509f6049b558e2ea7ccdacb
ILYA <106765406+D15ND@users.noreply.github.com>
2026-07-11 21:11:38 +0300

    chore: add ignore for nonused elements

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -9,6 +9,13 @@ const eslintConfig = defineConfig([
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
     },
   },
 ]);
